9f72d005c3943c5e0ff803bf4c08e144fc0b8814
nazanbahar <77123580+nazanbahar@users.noreply.github.com>
2021-01-27 20:53:29 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using GameProgramManagementDemo.Abstract;
+using GameProgramManagementDemo.Adapters;
+using GameProgramManagementDemo.Concrete;
+using System;
+
+namespace GameProgramManagement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //mernis sadece doğum yılını kontrol ediyor.
+            BaseCustomerManager customerManager = new PersonCustomerManager(new MernisServiceAdapter());
+
+
+            customerManager.Save(new Customer
+            {
+                NationalityId = 65903045602,
+                FirstName = "Can",
+                LastName = "Bahar",
+                DateOfBirthYear = 1974
+            });
+
+        }
+    }
+}
